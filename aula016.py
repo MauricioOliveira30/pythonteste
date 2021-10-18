e924677702b54e15e65842fda996565998d5b7a0
MauricioOliveira30 <66374666+MauricioOliveira30@users.noreply.github.com>
2021-10-18 20:45:55 -0300

    Exercícios de python

--- aula016.py
+++ aula016.py
@@ -1,2 +1,8 @@
-pessoa=('Gustavo',39,'M',99.88)
-print(pessoa)
+valores = list()
+for cont  in range(0,5):
+valores.append(5)
+valores.append(9)
+valores.append(4)
+for c,v in enumerate(valores):
+    print(f'Na posição {c} encotrei o valor {v}')
+print('Cheguei ao final da lista')
--- aula017.py
+++ aula017.py
@@ -1,5 +1,16 @@
-a=[2,3,4,7]
-b=a[:]
-b[2]=8
-print(f'Lista A:{a}')
-print(f'Lista B:{b}')
+galera =list()
+dado=list()
+totmai=totmen=0
+for c in range(0,3):
+    dado.append(str(input('Nome:')))
+    dado.append(int(input('Idade:')))
+    galera.append(dado[:])
+    dado.clear()
+for p in galera:
+       if p[1]>=21:
+           print(f'{p[0]} é maior de idade.')
+           totmai+=1
+       else:
+        print(f'{p[0]} é menor de idade.')
+        totmen+=1
+print(f'Temos {totmai} maiores e {totmen} menores de idade.')
